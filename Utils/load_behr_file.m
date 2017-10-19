5357bd66807e23c00eb3fe54e50970dab42ac6ea
Josh Laughner <joshlaugh5@gmail.com>
2017-10-19 15:18:22 -0700

    load_behr_file.m updated to deal with the OMI_BEHR-prof_region_yyyymmdd.mat file naming convention

--- Utils/load_behr_file.m
+++ Utils/load_behr_file.m
@@ -1,15 +1,20 @@
-function [ varargout ] = load_behr_file( file_date )
+function [ varargout ] = load_behr_file( file_date, prof_mode, region )
 %LOAD_BEHR_FILE Convenience function to load a BEHR file for a given date
 %
-%   LOAD_BEHR_FILE( FILE_DATE ) Loads the BEHR .mat file for FILE_DATE from
-%   the standard behr_mat_dir given in behr_paths. Places the variables
-%   Data and OMI directly in the base workspace.
+%   LOAD_BEHR_FILE( FILE_DATE, PROF_MODE, REGION ) Loads the BEHR .mat file
+%   for FILE_DATE processed with PROF_MODE profiles (usually 'daily' or
+%   'monthly') for the region REGION (default is 'us') from the standard
+%   behr_mat_dir given in behr_paths. Places the variables Data and OMI
+%   directly in the base workspace.
 %
 %   [ Data, OMI ] = LOAD_BEHR_FILE( FILE_DATE ) Returns Data and OMI as
 %   outputs instead.
 
+if ~exist('region','var')
+    region = 'us';
+end
 
-behr_file = fullfile(behr_paths.behr_mat_dir, behr_filename(file_date));
+behr_file = fullfile(behr_paths.behr_mat_dir, behr_filename(file_date, prof_mode, region));
 D = load(behr_file);
 if nargout == 0
     Data = D.Data;
