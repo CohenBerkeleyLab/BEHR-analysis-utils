be3a5721bec171fdddc1d369849114daf417cfff
Josh Laughner <first.tempora@gmail.com>
2018-11-13 23:37:29 -0800

    invert_vcd_to_mixing_ratio returns scale factor

--- Utils/invert_vcd_to_mixing_ratio.m
+++ Utils/invert_vcd_to_mixing_ratio.m
@@ -1,4 +1,4 @@
-function inverted_profiles = invert_vcd_to_mixing_ratio(vcds, profiles, prof_pres)
+function [inverted_profiles, scale_factor] = invert_vcd_to_mixing_ratio(vcds, profiles, prof_pres)
 % INVERT_VCD_TO_MIXING_RATIOS Calculate mixing ratios from a VCD and profile
 %   INVERTED_PROFILES = INVERT_VCD_TO_MIXING_RATIO( VCDS, PROFILES,
 %   PROF_PRES) calculates a profile consistent with the shape factor
@@ -20,21 +20,24 @@ function inverted_profiles = invert_vcd_to_mixing_ratio(vcds, profiles, prof_pre
 
 E = JLLErrors;
 
-sz_vcds = size(vcds);
 sz_profiles = size(profiles);
 sz_pres = size(prof_pres);
 
-if ~isequal(sz_vcds, sz_profiles(2:end))
+% Can't use isequal(size(vcds), sz_profiles(2:end)) because if the VCDs are
+% a vector and the profiles a 2D matrix, then it'll fail ([1 n] != [n]).
+if numel(vcds) ~= prod(sz_profiles(2:end))
     E.badinput('The size of VCDS and the size of PROFILES after the first dimension must be the same')
 elseif ~isequal(sz_profiles, sz_pres)
     E.badinput('The size of PROFILES and PROF_PRES must be the same')
 end
 
 inverted_profiles = nan(size(profiles));
+scale_factor = nan(size(vcds));
 
 for i_prof = 1:numel(vcds)
     sf = shape_factor(prof_pres(:,i_prof), profiles(:,i_prof), 'mode', 'mixing ratio');
     inverted_profiles(:,i_prof) = vcds(i_prof) .* sf;
+    scale_factor(i_prof) = nanmean(inverted_profiles(:,i_prof) ./ profiles(:,i_prof));
 end
 
 
